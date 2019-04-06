8b5b13d267b9fb79e1c063be4c1eece8219df688
thompsonaron <athomps@racunarstvo.hr>
2019-04-06 15:28:14 +0200

    SkoroPaBattleRoyale i DZ

--- Predavanje4_1/Source.cpp
+++ Predavanje4_1/Source.cpp
@@ -1,35 +1,104 @@
 #include <iostream>
 #include <string>
+
+class Player
+{
+public:
+	int x, y, velocity, hp, strenght;
+	std::string userName;
+
+	Player( int positionX, int positionY, int currentVelocity, int hitPoints) 
+	{
+		x = positionX;
+		y = positionY;
+		velocity = currentVelocity;
+		hp = hitPoints;
+	}
+
+	void move() 
+	{
+		x += velocity;
+		y += velocity;
+	}
+
+	void damage(int ammount)
+	{
+		hp -= ammount;
+	}
+
+};
+
+
+
+void move(int& x,int& y, int velocity) 
+{
+	x += velocity;
+	y += velocity;
+}
 int main()
 {
-	int number = 5;
-	int& tracker = number;
-	int* ptr = &number;
-	std::string* stringptr;
-	std::string someWord = "nesto";
-	stringptr = &someWord;
+	Player playerOne(0, 0, 10, 30);
+	playerOne.userName = "Trol 1";
+	playerOne.strenght = 12;
 
-	int** doubleptr = &ptr;
+	Player playerTwo(10, 15, 10, 30);
+	playerTwo.userName = "Smurf 1";
+	playerTwo.strenght = 11;
 
-	number = 20;
-	tracker = 4;
+	playerOne.move();
+	playerTwo.move();
+	
+	std::string losingPlayer;
+	std::string winningPlayer;
 
+	while (playerOne.hp > 0 && playerTwo.hp > 0)
+	{
+		std::cout << "Choose which player you want to hit: ";
+		int check;
+		std::cin >> check;
+		std::cout << std::endl;
 
+		std::cin.ignore(1000, '\n');
+		std::cin.clear();
 
+		switch (check)
+		{
+		case 1:
+			playerOne.damage(playerTwo.strenght);
+			std::cout << "Player one lost 5 hitpoints " << std::endl;
+			break;
+		case 2:
+			playerTwo.damage(playerOne.strenght);
+			std::cout << "Player two lost 5 hitpoints " << std::endl;
+			break;
+		default:
+			break;
+		}
+		std::cout << "Player one hitpoints: " << playerOne.hp << std::endl;
+		std::cout << "Player two hitpoints: " << playerTwo.hp << std::endl;
 
+		if (playerOne.hp <= 0)
+		{
+			losingPlayer = playerOne.userName;
+			winningPlayer = playerTwo.userName;
+		}
+		else if (playerTwo.hp <= 0)
+		{
+			losingPlayer = playerTwo.userName;
+			winningPlayer = playerOne.userName;
+		}
+	}
+	
 
+	std::cout << std::endl << losingPlayer << " lost the game. "<< std::endl;
 
-	std::cout << "Hello world" << std::endl;
-	std::cout << number << std::endl;
-	std::cout << tracker << std::endl;
-	std::cout << *ptr << std::endl;
-	std::cout << ptr << std::endl;
-	std::cout << "stringptr:" << stringptr << std::endl;
-	std::cout << "dereference stringptr:" << (*stringptr) << std::endl;
-	*stringptr = "nesto drugo";
-	std::cout << "dereference doradjen stringptr:" << (*stringptr) << std::endl;
-	std::cout << "double ptr adress: " << (**doubleptr) << std::endl;
-	std::cout << "double referenced int je: " << (**doubleptr) << std::endl;
+	std::cout << winningPlayer << " has won the game" << std::endl;
+
 	std::cin.get();
-
+	system("pause");
 }
+
+// 1) napraviti treceg player, postaviti mogucnost za biranje usernamea (korisnik sam unosi username)
+// 2) prosiriti mogucnost damageanja playera tako da se bira koji ce kojeg udariti.
+// 3) dodati dinamicki broj igraca (nije jos obavezno)
+
